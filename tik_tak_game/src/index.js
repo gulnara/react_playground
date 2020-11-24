38b0884037033a382fda71a2da02e4a257eda15e
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-11-24 15:16:31 -0800

    moved status logic into game from board

--- tik_tak_game/src/index.js
+++ tik_tak_game/src/index.js
@@ -27,21 +27,12 @@ class Board extends React.Component {
 
   renderSquare(i) {
     return <Square 
-      value={this.state.squares[i]}
-      onClick={() => this.handlelClick(i)}
+      value={this.props.squares[i]}
+      onClick={() => this.props.onClick(i)}
     />;
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
-
     return (
       <div>
         <div className="status">{status}</div>
@@ -77,13 +68,26 @@ class Game extends React.Component {
   }
 
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handlelClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
