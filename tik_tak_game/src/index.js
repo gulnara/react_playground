53eb446f36bbc6dc415ef106e72e36e1a5258c99
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-11-25 14:55:53 -0800

    added jump to method

--- tik_tak_game/src/index.js
+++ tik_tak_game/src/index.js
@@ -52,8 +52,16 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
+      stepNumber: 0,
       xIsNext: true,
     }
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext:(step % 2) === 0,
+    })
   }
 
   handlelClick(i) {
